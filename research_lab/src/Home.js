071f19b602ab548ab6150f0d9bcb53edf124bbcb
Matt Medlin <50458786+mattmedlin@users.noreply.github.com>
2022-04-05 18:13:46 -0500

    home almost done

--- research_lab/src/Home.js
+++ research_lab/src/Home.js
@@ -1,28 +1,33 @@
 import React, { Component } from "react";
-import {
-  TwitterTimelineEmbed,
-  TwitterShareButton,
-  TwitterFollowButton,
-  TwitterHashtagButton,
-  TwitterMentionButton,
-  TwitterTweetEmbed,
-  TwitterMomentShare,
-  TwitterDMButton,
-  TwitterVideoEmbed,
-  TwitterOnAirButton,
-} from "react-twitter-embed";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <h2>HOME</h2>
-        <div>
-          <div className="twitter-feed" style={{ textAlign: "center" }}>
+        <h2>Home</h2>
+        <div className="row">
+          <div className="column">
+            <p
+              style={{
+                textAlign: "left",
+                width: "80%",
+                paddingLeft: "5%",
+                paddingTop: "2%",
+              }}
+            >
+              Cras facilisis urna ornare ex volutpat, et convallis erat
+              elementum. Ut aliquam, ipsum vitae gravida suscipit, metus dui
+              bibendum est, eget rhoncus nibh metus nec massa. Maecenas
+              hendrerit laoreet augue nec molestie. Cum sociis natoque penatibus
+              et magnis dis parturient montes, nascetur ridiculus mus.
+            </p>
+          </div>
+          <div className="column" style={{ paddingRight: "70px" }}>
             <TwitterTimelineEmbed
               sourceType="profile"
-              screenName="saurabhnemade"
-              options={{ width: 700 }}
+              screenName="_mattmedlin"
+              options={{ width: "1000", height: "800" }}
             />
           </div>
         </div>
